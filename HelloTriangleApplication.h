371068b5b250bc0473627b38990af7c132657364
testFire0 <ashaxe22@gmail.com>
2023-11-16 13:58:44 +0100

    Staging Buffer 1.0

--- HelloTriangleApplication.h
+++ HelloTriangleApplication.h
@@ -184,6 +184,8 @@ private:
 
 	void createVertexBuffer();
 	uint32_t findMemoryType(uint32_t typeFilter, VkMemoryPropertyFlags properties);
+	void createBuffer(VkDeviceSize size, VkBufferUsageFlags usage, VkMemoryPropertyFlags properties, VkBuffer& buffer, VkDeviceMemory& bufferMemory);
+	void copyBuffer(VkBuffer srcBuffer, VkBuffer dstBuffer, VkDeviceSize size);
 
 	static void framebufferResizeCallback(GLFWwindow* window, int width, int height);
 };
